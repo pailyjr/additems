1c90516da76fa02196db4e6a20056d03cf4af400
pailyjr <alans.sunny@gmail.com>
2022-04-02 00:36:50 +0530

    new

--- Src/Screens/AddItems/AddItems.js
+++ Src/Screens/AddItems/AddItems.js
@@ -6,16 +6,21 @@ import ItemsBody from '../../Components/ItemsBody/ItemsBody'
 const AddItems = () => {
 
     //declaring states
-    const [searchVal, setSearchVal] = useState("")
-    const [items, setItems] = useState(["Milk 123","Coffee 776","Oranges 241","Bread 882"])
-    const [searchedItems, setSearchedItems] = useState([])
+    const [searchVal, setSearchVal] = useState("") //Search input
+    const [items, setItems] = useState(["Milk 123","Coffee 776","Oranges 241","Bread 882"])   //initial items
+    const [searchedItems, setSearchedItems] = useState([])   //searched items (initially everything will be displayed as seachval = '')
     const [backeditems] = useState(["blanket","magnet","phone","vase","socks","nail file","spring","floor","glass","shoe lace","pool stick","clock","purse","perfume","chalk","mirror","boom box","bookmark","cookie jar","chocolate","washing machine","air freshener","balloon","soda can",])
 
     //add item function definition
     const additem = () => {
         let len = Math.floor(Math.random() * backeditems.length)
+        console.log(len);
+        //To add id to each common items to separate them
         let itid = Math.floor(Math.random() * (1000-len))
+        console.log(itid);
+        //New item will be created with item name and unique id
         let newitem = backeditems[len] + " " + itid;
+        console.log(newitem);
         setItems(preitems=>[newitem,...preitems])
     }
 
